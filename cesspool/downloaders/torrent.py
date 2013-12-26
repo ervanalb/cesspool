24543d36ddadd50d963b35bc274cdd8578ee2648
Zach Banks <zbanks@mit.edu>
2013-12-26 05:41:43 -0500

    Basic UI working

--- cesspool/downloaders/torrent.py
+++ cesspool/downloaders/torrent.py
@@ -66,7 +66,8 @@ class TorrentDownloader(Downloader):
 
 	def add_torrent(self,child):
 		params={'save_path': child.dlpath}
-		child.handle=lt.add_magnet_uri(self.session,child.url,params)
+		print self.session, child.url, params
+		child.handle=lt.add_magnet_uri(self.session,str(child.url),params)
 
 	def remove_torrent(self,child):
 		self.session.remove_torrent(child.handle)
